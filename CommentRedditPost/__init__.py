3302e590a079e3c9b164fb5e0e27dcdc78471b05
Duarte Ferreira Dias <dudias@microsoft.com>
2021-11-24 15:45:59 +0000

    need to fix auth failure

--- CommentRedditPost/__init__.py
+++ CommentRedditPost/__init__.py
@@ -1,9 +1,36 @@
 import datetime
 import logging
+import os
+import praw
 
 import azure.functions as func
 
+def bot_login():
+	print("Logging in...")
 
+	r = praw.Reddit(username = os.environ["username"],
+				password = os.environ["password"],
+				client_id = os.environ["client_id"],
+				client_secret = os.environ["client_secret"],
+				user_agent = "The Reddit Commenter v1.0")
+
+	print("Logged in!")
+
+	return r
+
+def get_last_posts(r, subreddit, limit):
+    subreddit = r.subreddit(subreddit)
+    posts = subreddit.new(limit=limit)
+    return posts
+
+def add_comment_to_post(r, post, comment):
+    post.reply(comment)
+
+def check_if_comment_exists(r, post, comment):
+    for comment in post.comments.list():
+        if comment.body == comment:
+            return True
+    return False
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
@@ -13,3 +40,20 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    # Create an instance of the Reddit API
+    r = bot_login()
+
+    # Get the last 10 posts from the subreddit
+    posts = get_last_posts(r, os.environ["subreddit"], 1)
+
+    # Loop through the posts
+    for post in posts:
+        # If the post is not a self post
+        if not post.is_self:
+            # If the post does not already have a comment
+            if not check_if_comment_exists(r, post, os.environ["comment"]):
+                # Upvote post
+                post.upvote()
+                # Add the comment
+                add_comment_to_post(r, post, os.environ["comment"])
